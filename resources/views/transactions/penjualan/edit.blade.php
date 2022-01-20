75acd0aa76b4174847f8f45651b490de21d7ac22
pramono <pramono6236@gmail.com>
2022-01-20 10:55:20 +0700

    merapikan code blade edit

--- resources/views/transactions/penjualan/edit.blade.php
+++ resources/views/transactions/penjualan/edit.blade.php
@@ -38,9 +38,11 @@
                         </tr>
                     </table>
                 </div>
+                {{-- /. card-cody end --}}
             </div>
+            {{-- /. card-end --}}
         </div>
-        {{-- /. col-6 --}}
+        {{-- col-12 end --}}
         <div class="col-12">
             <div class="card">
                 <div class="card-header bg-secondary">
@@ -48,7 +50,7 @@
                 </div>
                 <div class="card-body">
                     <div class="mb-3">
-                        <select class="form-control form-control-sm" v-on:change="orderBaru($event)">
+                        <select class="form-control form-control-sm" @change="orderBaru($event)">
                             <option value="" selected >Tambah Produk</option>
                             @foreach ($products as $product)
                                 <option value="{{$product->id}}">{{$product->code}} | {{$product->name}} ( Rp {{$product->price}} )</option>
@@ -75,11 +77,11 @@
                                 <td>@{{product.name}}</td>
                                 <td>@{{product.price}}</td>
                                 <td>
-                                    <input type="number" :name="'quantity[' + product.product_id +']'" class="form-control form-control-sm qty" :value="product.qty" @keyup="updateSubtotal($event)">
+                                    <input type="number" :name="'quantity[' + product.product_id +']'" class="form-control form-control-sm qty" :value="product.qty" @keyup="editQuantity($event)">
                                 </td>
                                 <td>@{{product.line_total}}</td>
                                 <td>
-                                    <button type="button" class="btn btn-sm btn-danger" v-on:click="hapusOrder(k, product)">
+                                    <button type="button" class="btn btn-sm btn-danger" @click="hapusOrder(k, product)">
                                         <i class="fas fa-trash"></i>
                                     </button>
                                 </td>
@@ -87,150 +89,181 @@
                         </tbody>
                         <tfoot class="text-right font-italic">
                             <tr>
-                                <td colspan="4">
-                                    <b>Total Tagihan</b>
-                                </td>
+                                <td colspan="4"><b>Total Tagihan</b></td>
                                 <td class="text-center">
-                                    <input type="text" class="form-control form-dontrol-sm text-center" :value="grandTotal">
+                                    <input type="text" class="form-control form-dontrol-sm text-center" :value="totalTagihan">
                                 </td>
                                 <td></td>
                             </tr>
                             <tr>
-                                <td colspan="4">
-                                    <b>Pembayaran</b>
-                                </td>
+                                <td colspan="4"><b>Pembayaran</b></td>
                                 <td class="text-center">
-                                    <input type="number" name="payment" id="payment" class="form-control form-control-sm text-center" :value="pembayaran">
+                                    <input type="number" name="payment" id="payment" class="form-control form-control-sm text-center" :value="transaksi.payment">
                                 </td>
                                 <td></td>
                             </tr>
                             <tr>
-                                <td colspan="4">
-                                    <b>Kembalian</b>
-                                </td>
+                                <td colspan="4"><b>Kembalian</b></td>
                                 <td class="text-center">
-                                    <input type="number" name="refund" id="refund" class="form-control form-control-sm text-center" :value="kembalian">
+                                    <input type="number" name="refund" id="refund" class="form-control form-control-sm text-center" :value="transaksi.refund">
                                 </td>
                                 <td></td>
                             </tr>
                         </tfoot>
                     </table>
                 </div>
+                {{-- /. card-body end --}}
             </div>
+            {{-- /. card end --}}
         </div>
-        {{-- /. col-12 --}}
+        {{-- /. col-12 end --}}
     </div>
+    {{-- /. row end --}}
     <div class="row">
         <div class="col-6">
             <a href="{{ url()->previous() }}" class="">kembali</a>
         </div>
+        {{-- /. col-6 end --}}
         <div class="col-6">
             <button type="submit" class="btn btn-primary float-right">Ubah</button>
         </div>
+        {{-- /. col-6 end --}}
     </div>
+    {{-- /. row end --}}
 </form>
+{{-- /. form end --}}
 @endsection
 
 @section('css')
- <!-- Select2 -->
-    <link rel="stylesheet" href="{{asset('adminLTE/plugins/select2/css/select2.min.css')}}">
-    <link rel="stylesheet" href="{{asset('adminLTE/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+    <!-- Select2 -->
+    {{-- <link rel="stylesheet" href="{{asset('adminLTE/plugins/select2/css/select2.min.css')}}"> --}}
+    {{-- <link rel="stylesheet" href="{{asset('adminLTE/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}"> --}}
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="{{asset('adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}"> --}}
 @endsection
 
 @push('script')
 <!-- Select2 -->
-    <script src="{{asset('adminLTE/plugins/select2/js/select2.full.min.js')}}"></script>
+    {{-- <script src="{{asset('adminLTE/plugins/select2/js/select2.full.min.js')}}"></script> --}}
     <!-- InputMask -->
-    <script src="{{asset('adminLTE/plugins/moment/moment.min.js')}}"></script>
+    {{-- <script src="{{asset('adminLTE/plugins/moment/moment.min.js')}}"></script> --}}
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{asset('adminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-
-    {{-- sweetalert CDN --}}
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    {{-- <script src="{{asset('adminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script> --}}
 
     <script>
         var app = new Vue({
-                    el: "#app",
-                    data: {
-                        produk: JSON.parse('{!! $products !!}'),
-                        penjualan: JSON.parse('{!! $transaction->products !!}'),
-                        grandTotal: 0,
-                        orders: [],
-                        pembayaran: JSON.parse('{!! $transaction->payment !!}'),
-                        kembalian: JSON.parse('{!! $transaction->refund !!}'),
-                    },
-                    mounted: function () {
-                        this.loadOrder(this.penjualan)
-                        this.hitungSubtotal()
-                    },
-                    methods: {
-                        loadOrder(obj) {
-                            for (let i = 0; i < obj.length; i++) {
-                                const element = obj[i];
-                                this.orders.push({
-                                    product_id: element.id,
-                                    code: element.code,
-                                    name: element.name,
-                                    price: element.price,
-                                    qty: element.pivot.quantity,
-                                    line_total: 0
-                                });
-                            }
-                        },
-                        orderBaru(e) {
-                            // ambil value sebagai id
-                            var id = e.target.value - 1;
-                            // filter produk berdasarkan id
-                            var selected = this.produk[id];
+            el: "#app",
+            data: {
+                produk: JSON.parse('{!! $products !!}'), // daftar produk untuk mengisi order
+                transaksi: JSON.parse('{!! $transaction !!}'), // transaksi yang dikirim dari controller  untuk mengisi form edit
+                totalTagihan: 0, // untuk mengisi total harga dari orders
+                orders: [], // untuk memanipulasi tabel order
+            },
+            mounted: function () {
+                this.nilaiDefault(this.transaksi.products) // isi form edit
+                this.hitungSubtotal() // hitung total harga order
+                // console.log(this.mataUang(10000)) testing method convert angka ke matauang
+            },
+            methods: {
+                // method nilai default form
+                nilaiDefault(transaksi) {
+                    // looping transaksi yanng dikirim dari parameter
+                    for (let i = 0; i < transaksi.length; i++) {
+                        // inisialisasi index transaksi
+                        const element = transaksi[i];
+                        // isi data orders dengan transaksi yang telah diinisialisasi
+                        this.orders.push({
+                            product_id: element.id,
+                            code: element.code,
+                            name: element.name,
+                            price: element.price,
+                            qty: element.pivot.quantity,
+                            line_total: 0
+                        });
+                    }
+                },
+                // method menamahkan order baru
+                orderBaru(e) {
+                    // ambil id produk yang dipilih
+                    var id = e.target.value - 1;
+                    // ambil data produk sesuai dengan id produk
+                    var selected = this.produk[id];
+                    // isi data orders
+                    this.orders.push({
+                        product_id: selected.id,
+                        code: selected.code,
+                        name: selected.name,
+                        price: selected.price,
+                        qty: 1,
+                        line_total: 0
+                    });
+                    // hitung harga total
+                    this.hitungSubtotal()
+                    // atur ulang data pembayaran dan kembalian menjadi 0
+                    this.transaksi.payment = 0;
+                    this.transaksi.refund = 0;
+                },
+                // method untuk menghapus order yang ada
+                hapusOrder(index, product) {
+                    // filter data orders sesuai dengan index dari parameter
+                    var idx = this.orders.indexOf(product);
+                    // console.log(idx, index); testing
 
-                            this.orders.push({
-                                product_id: selected.id,
-                                code: selected.code,
-                                name: selected.name,
-                                price: selected.price,
-                                qty: 1,
-                                line_total: 0
-                            });
+                    if (idx > -1) {
+                        this.orders.splice(idx, 1);
+                    }
+                    // hitung ulang harga total
+                    this.hitungSubtotal();
+                    // atur ulang data pembayaran dan kembalian menjadi 0
+                    this.transaksi.payment = 0;
+                    this.transaksi.refund = 0;
+                },
+                // method untuk menghitung subtotal harga masing-masing orders
+                hitungSubtotal() {
+                    // inisialisasi data orders
+                    var daftarOrder = this.orders;
+                    // atur ulang total tagihan
+                    this.totalTagihan = 0;
+                    // looping data orders
+                    for (let i = 0; i < daftarOrder.length; i++) {
+                        // ambil orders ke-n
+                        const element = daftarOrder[i];
+                        // ambil harga order ke-n
+                        const harga = element.price;
+                        // ambil quantity orders ke-n
+                        const qty = element.qty;
+                        // kalikan harga ke-n dengan quantity ke-n dari orders ke-n
+                        const subtotal = parseInt(harga) * parseInt(qty);
+                        // isi subtotal dari orders ke-n dengan subtotal
+                        this.orders[i].line_total = subtotal;
+                        // tambah data total tagihan dengan subtotal ke-n
+                        this.totalTagihan += subtotal;
+                    }
+                },
+                // method untuk menghandle perubahan nilai quantity
+                editQuantity(event) {
+                    // ambil semua elemen quantity
+                    var element = $(".qty");
+                    // ambil nilai quantity yang diisi
+                    var nilai = event.target.value;
+                    // inisialisasi element yang diubah
+                    var id = Array.from(element).indexOf(event.target)
+                    // ubah data orders yang sudah diinisialisasi dengan nilai
+                    this.orders[id].qty = nilai;
+                    // console.log(this.orders[id].qty)
+                    // hitung ulang total harga
+                    this.hitungSubtotal();
+                    // atur ulang nilai pembayaran dan kembalian
+                    this.transaksi.payment = 0;
+                    this.transaksi.refund = 0;
+                },
+                mataUang(number) {
+                    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                }
+            },
+            computed: {
 
-                            this.hitungSubtotal()
-                        },
-                        hapusOrder(index, product) {
-                            var idx = this.orders.indexOf(product);
-                            console.log(idx, index);
-                            if (idx > -1) {
-                                this.orders.splice(idx, 1);
-                            }
-                            this.hitungSubtotal()
-                        },
-                        hitungSubtotal() {
-                            var semuaproduk = this.orders;
-                            this.grandTotal = 0;
-                            for (let p = 0; p < semuaproduk.length; p++) {
-                                const element = semuaproduk[p];
-                                const harga = element.price;
-                                const qty = element.qty;
-                                const subtotal = parseInt(harga) * parseInt(qty);
-                                this.orders[p].line_total = subtotal;
-                                this.grandTotal += subtotal;
-                            }
-                        },
-                        hitungTotal(total, num) {
-                            return total + num;
-                        },
-                        updateSubtotal(event) {
-                            var element = $(".qty");
-                            var nilai = event.target.value;
-                            var id = Array.from(element).indexOf(event.target)
-                            this.orders[id].qty = nilai;
-                            console.log(this.orders[id].qty)
-                            this.hitungSubtotal()
-                        },
-                    },
-                    computed: {
-
-                    },
-                })
+            },
+        })
     </script>
 @endpush
