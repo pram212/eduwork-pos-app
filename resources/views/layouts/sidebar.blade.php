fe29f5183069d66d7c4417657e6c3f6cac177a58
pramono6236 <pramono6236@gmail.com>
2023-08-13 00:33:01 +0700

    fix bugs image profile

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,9 +8,6 @@
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="https://placeimg.com/80/80/people" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
           <a href="{{ route( 'users.edit', ['user' => Auth::user()->id ] ) }}" class="d-block">{{ Auth::user()->name }}</a>
         </div>
